f2459707bcf2014056ef44d94f8ce7815215f74c
Doo-Dong <ehd6754@gmail.com>
2021-03-26 12:33:07 +0900

    레이아웃 잡는 중 ...

--- app/src/main/java/com/learn4/LoginActivity.java
+++ app/src/main/java/com/learn4/LoginActivity.java
@@ -2,13 +2,42 @@ package com.learn4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText login_id_input_box;
+    ImageView auto_login;
+    ImageButton login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_id_input_box = findViewById(R.id.login_id_input_box);
+        auto_login = findViewById(R.id.auto_login);
+        login_btn = findViewById(R.id.login_btn);
+
+        auto_login.setOnClickListener(v -> {
+            runOnUiThread(() -> {
+                if (!v.isSelected()) {
+                    v.setBackground(getResources().getDrawable(R.drawable.auto_login_));
+                    v.setSelected(true);
+                } else {
+                    v.setBackground(getResources().getDrawable(R.drawable.auto_login_off));
+                    v.setSelected(false);
+                }
+            });
+        });
+
+        login_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
