b66562512650db40f2a93ac9680184d22f0ee2cb
Doo-Dong <ehd6754@gmail.com>
2021-05-07 11:48:48 +0900

    임시 커밋

--- app/src/main/java/com/corporation8793/mUsbReceiver.java
+++ app/src/main/java/com/corporation8793/mUsbReceiver.java
@@ -7,17 +7,23 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbRequest;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 
 import com.physicaloid.lib.Boards;
 import com.physicaloid.lib.Physicaloid;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class mUsbReceiver extends BroadcastReceiver {
     Context ct;
     Physicaloid physicaloid;
+    UsbDevice bigBoard;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -25,16 +31,28 @@ public class mUsbReceiver extends BroadcastReceiver {
         ct = context;
         physicaloid = new Physicaloid(context);
 
+        UsbManager manager = (UsbManager) context.getSystemService(context.USB_SERVICE);
+
+        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+
+        if(deviceList.isEmpty()){
+            Log.e("USB 감지 : ", "isempty 디바이스 리스트");
+        }
+        else {
+            Set<String> keys = deviceList.keySet();
+            for (String key : keys) {
+                bigBoard = deviceList.get(key);
+            }
+        }
+
         if(action.equals("android.hardware.usb.action.USB_DEVICE_ATTACHED")) {
             // USB was connected
             Log.d("USB 감지 : ", "연결연결");
 
             physicaloid.open();
-
         } else if (action.equals("android.hardware.usb.action.USB_DEVICE_DETACHED")) {
             // USB was disconnected
             Log.d("USB 감지 : ", "실패실패");
-
         }
     }
 }
