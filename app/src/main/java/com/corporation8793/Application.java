702397dfad8f7c25bb37c88a8bf1a8c828ffc9f7
Kim <divingsky@hanmail.net>
2022-01-10 15:13:34 +0900

    로딩중 화면 표시해줄것 구현

--- app/src/main/java/com/corporation8793/Application.java
+++ app/src/main/java/com/corporation8793/Application.java
@@ -1,11 +1,15 @@
 package com.corporation8793;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+
+import com.corporation8793.dialog.ProgressDialog;
 
 public class Application extends android.app.Application {
     public static int indicator_index = -1;
     Context context;
     private LogHelper logHelper;
+    private ProgressDialog loadingDialog;
 
     public com.corporation8793.Application getInstance(Context context){
         this.context = context;
@@ -19,6 +23,22 @@ public class Application extends android.app.Application {
         Thread.setDefaultUncaughtExceptionHandler(logHelper);
 
 
+    }
+
+
+    public void showLoadingScreen(Context context){
+        if(context != null){
+            loadingDialog = new ProgressDialog(context);
+            loadingDialog.setContentView(R.layout.dialog_progress);
+            loadingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+            loadingDialog.show();
+        }
+    }
+
+    public void hideLoadingScreen(){
+        if(loadingDialog != null){
+            loadingDialog.dismiss();
+        }
     }
 
 
