3e0468a7fdcdf46c96e1f8d070b3de3e3bbe6a9b
gyeong <gyeongyeongi22@gmail.com>
2022-01-21 16:25:54 +0900

    pull전 commit

--- app/src/main/java/com/corporation8793/Application.java
+++ app/src/main/java/com/corporation8793/Application.java
@@ -5,9 +5,11 @@ import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 
 import com.corporation8793.dialog.ProgressDialog;
+import com.google.blockly.model.Input;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 import jxl.Sheet;
 import jxl.Workbook;
@@ -17,7 +19,10 @@ public class Application extends android.app.Application {
     Context context;
     private LogHelper logHelper;
     private ProgressDialog loadingDialog;
-    Excel excel;
+    InputStream is;
+    Workbook workbook;
+    public HashMap<String,String[]> components = new HashMap<>();
+
 
     public com.corporation8793.Application getInstance(Context context){
         this.context = context;
@@ -29,8 +34,9 @@ public class Application extends android.app.Application {
         super.onCreate();
         logHelper = new LogHelper();
         Thread.setDefaultUncaughtExceptionHandler(logHelper);
-        excel = new Excel(context, "contents_mode_info.xls");
-        excel.readComponent();
+        readComponent();
+        readContents();
+
     }
 
 
@@ -50,8 +56,65 @@ public class Application extends android.app.Application {
     }
 
 
+    public void readComponent(){
+        try{
+            is = getBaseContext().getResources().getAssets().open("contents_mode_info.xls");
+            workbook = Workbook.getWorkbook(is);
+            if (workbook != null){
+                Sheet sheet = workbook.getSheet(1);
+                if (sheet != null){
+                    int colTotal = sheet.getColumns();
+                    int rowIndexStart = 1;
+                    int rowTotal = sheet.getColumn(colTotal-1).length;
 
+                    String num;
 
+                    for (int row = rowIndexStart; row<rowTotal; row++){
+                        num = sheet.getCell(2,row).getContents();
+
+                        String [] component_content = new String[2];
+                        for (int col=3; col<5; col++){
+                            component_content[col - 3] = sheet.getCell(col,row).getContents();
+                            Log.e("content check ",component_content[col - 3]);
+                        }
+                        components.put(num,component_content);
+                    }
+
+                }
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void readContents(){
+        try{
+            is = getBaseContext().getResources().getAssets().open("contents_mode_info.xls");
+            workbook = Workbook.getWorkbook(is);
+            if (workbook != null){
+                Sheet sheet = workbook.getSheet(0);
+                if (sheet != null){
+                    int colTotal = sheet.getColumns();
+                    int rowIndexStart = 2;
+                    int rowTotal = sheet.getColumn(colTotal-1).length;
+
+                    StringBuilder sb;
+                    for (int row = rowIndexStart; row<rowTotal; row++){
+                        sb = new StringBuilder();
+
+                        for (int col=0; col<colTotal; col++){
+                            String contents = sheet.getCell(col,row).getContents();
+
+                            Log.e("col ", contents);
+                        }
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 
 }
