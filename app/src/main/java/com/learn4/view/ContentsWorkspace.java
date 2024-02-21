300e4715048e3b0702c140790de3979ecf26068a
KSY9808 <kimsy9808@naver.com>
2024-02-21 12:46:01 +0900

    [ADD] 콘텐츠 모드 워크스페이스 화면 구성중

---
+++ app/src/main/java/com/learn4/view/ContentsWorkspace.java
@@ -0,0 +1,16 @@
+package com.learn4.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.learn4.R;
+
+public class ContentsWorkspace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contents_workspace);
+    }
+}
