0d68328d4cff646f689debc0f796c76756db69b1
gyeong <gyeongyeongi22@gmail.com>
2024-03-08 16:13:28 +0900

    [PULL] 풀 전 커밋

--- app/src/main/java/com/learn4/view/contents/LevelAdapter.java
+++ app/src/main/java/com/learn4/view/contents/LevelAdapter.java
@@ -12,12 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.learn4.R;
 import com.learn4.data.dto.Level;
 import com.learn4.view.recyclerview.HorizonViewHolder;
+import com.learn4.view.recyclerview.ItemDecoration;
 import com.learn4.view.recyclerview.RecyclerDecoration;
 
 import java.util.ArrayList;
@@ -49,9 +51,12 @@ public class LevelAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
+
         ((HorizonViewHolder) holder).recyclerView.setAdapter(new LevelChapterAdapter(context, levels.get(position).chapters, width));
-        ((HorizonViewHolder) holder).recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+//        ((HorizonViewHolder) holder).recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        ((HorizonViewHolder) holder).recyclerView.setLayoutManager(new GridLayoutManager(context,4));
         ((HorizonViewHolder) holder).recyclerView.setHasFixedSize(true);
+        ((HorizonViewHolder) holder).recyclerView.addItemDecoration(new ItemDecoration(context));
         String [] level_split = levels.get(position).level.split(" ");
         SpannableStringBuilder ssb = new SpannableStringBuilder(levels.get(position).level);
         ssb.setSpan(new ForegroundColorSpan(Color.parseColor("#ff953a")), 5, 7, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
